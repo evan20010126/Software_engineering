3be84196405577143d6c0f3bd6227d30cf041a47
Tzuyu <fcebk17@gmail.com>
2021-12-26 23:56:41 +0800

    modify Line3

--- 軟體工程專案/php/searching.php
+++ 軟體工程專案/php/searching.php
@@ -1,6 +1,6 @@
 <?php
-	//連線到資料庫 (這邊暫定資料庫名稱為edit.php)
-	require_once('edit.php');
+	//連線到資料庫 
+	require_once('db_conn_software.php');
 	if(empty($_POST['searching_bar'])){ //SDD文件沒有搜尋欄這個資料結構 
         echo '請輸入欲查詢品項名稱<br>'; //這邊先給它叫做searching_bar
 		exit(); //啊我不知道這個echo有沒有存在的必要@@
@@ -26,6 +26,6 @@
         die($edit->error);
     }
 
-    header("Location: ");//會自動跳轉的地方
+    header("Location: ");//會自動跳轉的地方 但不知道要跳到哪個頁面暫時為空
 
 ?>
