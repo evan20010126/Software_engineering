78923f28fbfb766a8bd1e00723415e6ffd71a545
fcebk17 <83528127+fcebk17@users.noreply.github.com>
2021-12-26 23:36:14 +0800

    Serching products infomation

---
+++ 軟體工程專案/php/searching.php
@@ -0,0 +1,31 @@
+<?php
+	//連線到資料庫 (這邊暫定資料庫名稱為edit.php)
+	require_once('edit.php');
+	if(empty($_POST['searching_bar'])){ //SDD文件沒有搜尋欄這個資料結構 
+        echo '請輸入欲查詢品項名稱<br>'; //這邊先給它叫做searching_bar
+		exit(); //啊我不知道這個echo有沒有存在的必要@@
+	}
+
+	$product_name = $_POST['product_name'];
+	$product_info = $_POST['product_info'];
+	$product_pic = $_POST['product_pic'];
+	$product_price = $_POST['product_price'];
+	$sql = sprintf(
+        //帶入參數: %s , %d
+        "SELECT product_name, product_info, product_pic, product_price FROM Product WHERE product_name ='product_name%' ",
+        $product_name,
+        $product_info,
+        $product_pic,
+		$product_price
+    );
+	echo $sql . '<br>';
+
+    //執行結果儲存再 $result這個變數中
+    $result = $edit -> query($sql);
+    if(!$result){
+        die($edit->error);
+    }
+
+    header("Location: ");//會自動跳轉的地方
+
+?>
