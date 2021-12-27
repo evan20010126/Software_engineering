97a41a9546876d238c069332bc1c5c0f2524356f
fcebk17 <83528127+fcebk17@users.noreply.github.com>
2021-12-27 20:37:23 +0800

    老闆端查看歷史訂單

---
+++ 軟體工程專案/php/boss_history_order.php
@@ -0,0 +1,10 @@
+<?php
+    include "db_conn_software.php";
+    
+    $query = ("SELECT order_id, user_account, product_list, cost, T_stamp FROM order_list WHERE is_finish=1");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(); //執行sql語法
+    $result = $stmt->fetchAll();
+    echo json_encode($result);
+?>
+
