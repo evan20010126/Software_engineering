4fa8418896bca28edd6e3dbbd0481201c423366a
fcebk17 <83528127+fcebk17@users.noreply.github.com>
2021-12-28 19:29:40 +0800

    這裡的概念很抽象 有問題再來找我 主要功能是將修改前的產品資訊傳到前端

---
+++ 軟體工程專案/php/edit_product_1.php
@@ -0,0 +1,12 @@
+<?php
+    include "db_conn_software.php";
+    
+    $object = $_REQUEST["q"]; //前端需要與後端參數對應，object指前端需要抓的產品資訊
+    $query = ("SELECT product_name, product_info, product_pic, product_price, product_supply FROM product WHERE product_name = $object");   
+     
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(); //執行sql語法
+    $result = $stmt->fetchAll();
+
+    echo json_encode($result);
+?>
