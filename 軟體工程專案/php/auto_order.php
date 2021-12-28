dc791878909dbd9bd6878a8df42092186d28360a
YiKaiWu1125 <500000000kai@gmail.com>
2021-12-29 05:15:02 +0800

    自動取eat
    function name: auto_order()
    參數: auto_order_id、user_account、boss_account、product_list、cost、week_day、time
    
    新增一筆資料到auto_order_list的table內。
    
    輸出:
     1 (成功新增)
     0 (新增失敗)
    
    NOTE:auto_order_id不可重複

---
+++ 軟體工程專案/php/auto_order.php
@@ -0,0 +1,25 @@
+<?php
+    include "db_conn_software.php";
+
+    $auto_order_id = $_REQUEST["auto_order_id"];//ex: "1";
+    $user_account = $_REQUEST["user_account"];//ex: "user";
+    $boss_account = $_REQUEST["boss_account"];//ex: "boss";
+    $product_list = $_REQUEST["product_list"];//ex: "Nothing";
+    $cost = $_REQUEST["cost"];"111";//ex: "438";
+    $week_day = $_REQUEST["week_day"];//ex: "0";
+    $time = $_REQUEST["time"];//ex: '1970-01-01 08:00:00';
+
+    try{
+        $query = ("INSERT INTO auto_order_list VALUES(?,?,?,?,?,?,?)");
+        $stmt = $db->prepare($query);    
+        $error = $stmt->execute(array($auto_order_id,$user_account,$boss_account,$product_list,$cost,$week_day,$time)); //執行sql語法
+        $result = $stmt->fetchAll();
+        $bool = 1;
+        echo json_encode($bool);
+    }catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        $bool = 0;
+        echo json_encode($bool);
+    }
+    //預設不用回傳值
+?>
+
