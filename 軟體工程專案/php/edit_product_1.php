010037961c279e09c9ee61dec78c1ecc0ae6b8ae
fcebk17 <fcebk17@gmail.com>
2021-12-29 03:45:52 +0800

    將修改前的產品資訊傳到前端

--- 軟體工程專案/php/edit_product_1.php
+++ 軟體工程專案/php/edit_product_1.php
@@ -1,8 +1,8 @@
 <?php
     include "db_conn_software.php";
     
-    $object = $_REQUEST["q"]; //前端需要與後端參數對應，object指前端需要抓的產品資訊
-    $query = ("SELECT product_name, product_info, product_pic, product_price, product_supply FROM product WHERE product_name = $object");   
+    $product_id = $_REQUEST["q"]; //前端需要與後端參數對應，object指前端需要抓的產品資訊
+    $query = ("SELECT product_name, product_info, product_pic, product_price, product_supply FROM product WHERE product_id = ?");   
      
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(); //執行sql語法
