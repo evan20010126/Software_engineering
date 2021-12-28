63d799fdcae3ae13d2287ff639a95e9eb239e50a
YiKaiWu1125 <500000000kai@gmail.com>
2021-12-29 04:16:22 +0800

    Update edit_product_1.php

--- 軟體工程專案/php/edit_product_1.php
+++ 軟體工程專案/php/edit_product_1.php
@@ -7,6 +7,6 @@
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(); //執行sql語法
     $result = $stmt->fetchAll();
-
+    
     echo json_encode($result);
 ?>
