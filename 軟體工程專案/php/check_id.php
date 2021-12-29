da42447e4e6df6338f2e19b4509f639cd2901ab1
YiKaiWu1125 <500000000kai@gmail.com>
2021-12-29 17:22:50 +0800

    in product use product_name to find ID
    input => product_name
    output => int

--- 軟體工程專案/php/check_id.php
+++ 軟體工程專案/php/check_id.php
@@ -1,11 +1,11 @@
 <?php
     include "db_conn_software.php";
-
-    $product_name =strval($_REQUEST['product_name']);
-    echo( $product_name);
-    $query = ("SELECT product_id FROM package WHERE product_name = ? ");
+    $pn =$_REQUEST['product_name'];
+    //echo( $pn);
+    $query = ("SELECT product_id FROM product WHERE product_name = ").$pn;
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute($product_name); //執行sql語法
+    $error = $stmt->execute(); //執行sql語法
     $result = $stmt->fetchAll();
-    echo json_encode($result);
+    $val = $result[0][0];
+    echo json_encode($val);
 ?>
