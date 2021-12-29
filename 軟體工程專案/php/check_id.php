b18e0daf9ac0aa59c250c7b98f32ce10b752ac9b
YiKaiWu1125 <94746828+YiKaiWu1125@users.noreply.github.com>
2021-12-29 12:46:34 +0800

    Update check_id.php

--- 軟體工程專案/php/check_id.php
+++ 軟體工程專案/php/check_id.php
@@ -1,12 +1,12 @@
 <?php
     include "db_conn_software.php";
-    
-    $product_name = $_REQUEST["product_name"];//ex: "1";
+    $product_name = $_REQUEST["product_name"];
+    //echo(product_name);
 
     $query = ("SELECT product_id FROM product WHERE product_name = ? ");   
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute($product_name); //執行sql語法
     $result = $stmt->fetchAll();
-    $val = $result[0][0];
-    echo json_encode($val);
+//$val = $result[0][0];
+    echo json_encode($result);
 ?>
