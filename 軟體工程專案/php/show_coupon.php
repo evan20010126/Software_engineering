461bc00c64336933007da71c3e126403434004ec
fcebk17 <83528127+fcebk17@users.noreply.github.com>
2021-12-29 18:34:22 +0800

    顯示優惠券的代碼與折扣

---
+++ 軟體工程專案/php/show_coupon.php
@@ -0,0 +1,10 @@
+<?php
+    include "db_conn_software.php";
+    
+    $query = ("SELECT coupon_code, is_persentoff FROM coupon");    
+    $stmt = $db->prepare($query);        //db為db_conn.php新建的連線物件 
+    $error = $stmt->execute();
+    $result = $stmt->fetchAll();        //將所有搜尋結果存於result
+    
+    echo json_encode($result);
+?>
