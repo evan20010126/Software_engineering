778abcd6659dd8cf35468792811e577476c8f4f1
YiKaiWu1125 <500000000kai@gmail.com>
2021-12-29 08:25:31 +0800

    package delete完成
    function_name: delete_package_product()
    
    傳入的參數: user_account、product_id: “產品編號(int)”
    
    需實踐的功能:
    刪除package table內的此產品
    //call back
    //-1 : //not find user
    // 0 : //have user but not find product;
    // 1 : //shopping car have >=2 product need to reduce it
    // 2 : //shopping car only one product need to delete it

--- 軟體工程專案/php/delete_package_product.php
+++ 軟體工程專案/php/delete_package_product.php
@@ -1,4 +1,4 @@
-<?php  //感覺edit_product_1&2可以合併 若需合併再告訴我
+<?php  
     include "db_conn_software.php";
     //input
     $user_account = $_REQUEST["user_account"];//"407";
@@ -8,9 +8,7 @@
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(array($user_account)); //執行sql語法
     $result = $stmt->fetchAll();
-    //echo($result[0][3]);
-    //echo json_encode($result);
-    //echo(" ");
+    
     if($result == NULL){
         $bool = -1;//not find user
         echo json_encode($bool);
@@ -25,27 +23,35 @@
             echo json_encode($bool);
         }
         else{
-            if($result[0][1]==NULL){//shopping car only one product need to delete it
+            if($result[0][1]==NULL){
                 $stmt = $db->prepare("delete from package where user_account = ?");
 	            $result = $stmt->execute(array($user_account));
-                $bool = 2;//have user but not find product;
+                $bool = 2;//shopping car only one product need to delete it
                 echo json_encode($bool);
             }       
-            else{//shopping car have >=2 product need to reduce it
+            else{
                 for(++$i;$i<=9;$i++){
                     $k = strval($i) ;
                     $k = "product_id0".$k;
                     $s="UPDATE package SET ".$k." = ? WHERE user_account =?";
                     $query = ($s);
-                    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-                    $error = $stmt->execute(array($result[0][$i],$user_account)); //執行sql語法
+                    $stmt = $db->prepare($query);    
+                    $error = $stmt->execute(array($result[0][$i],$user_account)); 
                 }
                 $query = ("UPDATE package SET product_id10 = ? WHERE user_account = ?");
-                $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-                $error = $stmt->execute(array(NULL,$user_account)); //執行sql語法
-                $bool = 1;//have user but not find product;
+                $stmt = $db->prepare($query);     
+                $error = $stmt->execute(array(NULL,$user_account)); 
+                $bool = 1;//shopping car have >=2 product need to reduce it
                 echo json_encode($bool);
             }
         }
     }
+//call back
+//-1 : //not find user
+// 0 : //have user but not find product;
+// 1 : //shopping car have >=2 product need to reduce it
+// 2 : //shopping car only one product need to delete it
 ?>
+
+
+
