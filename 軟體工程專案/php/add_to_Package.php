2ad242a4abcb57c5d4645cd22ddc5098c7475a6d
YiKaiWu1125 <94746828+YiKaiWu1125@users.noreply.github.com>
2021-12-29 11:13:59 +0800

    新增食物到購物車(完工)
    function_name: add_to_Package()
    
    傳入的參數: user_account、product_id: “產品編號(int)”、T_stamp(時間)
    
    需實踐的功能:
    加此商品進去購物車的table(Package schema) schema有的資訊都要在php處理 丟資料進去schema
    //call back
    //-1 : //full can't put any thing
    // 0 : //not find user
    // 1 : //have user update product

---
+++ 軟體工程專案/php/add_to_Package.php
@@ -0,0 +1,67 @@
+<?php  
+    include "db_conn_software.php";
+    //input
+    $user_account = $_REQUEST["user_account"];//"666";
+    $product_id = $_REQUEST["product_id"];//5;
+    $T_stamp = $_REQUEST["T_stamp"];//'1970-01-01 08:00:00';
+
+    //check cost
+    $query = ("SELECT  product_price FROM product WHERE product_id = ?");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($product_id)); //執行sql語法
+    $result = $stmt->fetchAll();
+    $cost = $result[0][0];
+
+    $query = ("SELECT  product_id01, product_id02, product_id03, product_id04, product_id05, product_id06, product_id07, product_id08, product_id09, product_id10 ,cost FROM package WHERE user_account = ?");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($user_account)); //執行sql語法
+    $result = $stmt->fetchAll();
+
+    if($result == NULL){
+        $query = ("INSERT INTO package (user_account, product_id01, cost, T_stamp)VALUES (?,?,?,?)");
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($user_account,$product_id,$cost,$T_stamp)); //執行sql語法
+        $bool = 0;//not find user creat
+        echo json_encode($bool);
+    }
+    else{
+        $i = 0;
+        for(;$i<10;$i++){
+            if($result[0][$i]==NULL)break;
+        }
+        if($i==10){
+            $bool = -1;//full can't put any thing
+            echo json_encode($bool);
+        }
+        else{
+            $i++;
+            $k = strval($i) ;
+            if($i<10){
+                $k = "product_id0".$k;
+            }
+            else{
+                $k = "product_id".$k;
+            }
+            $s="UPDATE package SET ".$k." = ? WHERE user_account =?";
+            $query = ($s);
+            $stmt = $db->prepare($query);    
+            $error = $stmt->execute(array($product_id , $user_account));
+            //recost
+            $cost+=$result[0][10];
+            $s="UPDATE package SET cost = ? WHERE user_account =?";
+            $query = ($s);
+            $stmt = $db->prepare($query);    
+            $error = $stmt->execute(array($cost , $user_account));
+
+            $bool = 1;//have user update product
+            echo json_encode($bool);
+        }
+    }
+//call back
+//-1 : //full can't put any thing
+// 0 : //not find user
+// 1 : //have user update product
+?>
+
+
+
