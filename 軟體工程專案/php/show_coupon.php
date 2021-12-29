5a2f4749bafa73dbd0143ff10744be39c3ac4795
fcebk17 <fcebk17@gmail.com>
2021-12-29 21:35:28 +0800

    新增了一個num

--- 軟體工程專案/php/show_coupon.php
+++ 軟體工程專案/php/show_coupon.php
@@ -1,7 +1,7 @@
 <?php
     include "db_conn_software.php";
     
-    $query = ("SELECT coupon_code, is_persentoff FROM coupon");    
+    $query = ("SELECT coupon_code, is_persentoff, num FROM coupon");    
     $stmt = $db->prepare($query);        //db為db_conn.php新建的連線物件 
     $error = $stmt->execute();
     $result = $stmt->fetchAll();        //將所有搜尋結果存於result
