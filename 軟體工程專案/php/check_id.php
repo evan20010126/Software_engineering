6c865fa3d24e24149702feac091f72ffb88e7841
fcebk17 <fcebk17@gmail.com>
2021-12-29 18:25:07 +0800

    應該能跑

--- 軟體工程專案/php/check_id.php
+++ 軟體工程專案/php/check_id.php
@@ -1,10 +1,10 @@
 <?php
     include "db_conn_software.php";
-    $pn =$_REQUEST['product_name'];
-    //echo( $pn);
-    $query = ("SELECT product_id FROM product WHERE product_name = ").$pn;
+    $product_name = $_REQUEST['product_name'];
+
+    $query = ("SELECT product_id FROM product WHERE product_name = ?");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(); //執行sql語法
+    $error = $stmt->execute(array($product_name)); //執行sql語法
     $result = $stmt->fetchAll();
     $val = $result[0][0];
     echo json_encode($val);
