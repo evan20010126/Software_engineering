d2bf1dad05ddd5afa05903d7d31faf8f955201d0
YiKaiWu1125 <94746828+YiKaiWu1125@users.noreply.github.com>
2021-12-29 13:17:41 +0800

    他壞掉了

--- 軟體工程專案/php/check_id.php
+++ 軟體工程專案/php/check_id.php
@@ -1,12 +1,11 @@
 <?php
     include "db_conn_software.php";
-    $product_name = $_REQUEST["product_name"];
-    //echo(product_name);
 
-    $query = ("SELECT product_id FROM product WHERE product_name = ? ");   
+    $product_name =strval($_REQUEST['product_name']);
+    echo( $product_name);
+    $query = ("SELECT product_id FROM package WHERE product_name = ? ");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute($product_name); //執行sql語法
     $result = $stmt->fetchAll();
-//$val = $result[0][0];
     echo json_encode($result);
 ?>
