3ece18f20d43ed2c6732a54b970ad54316139d0f
fcebk17 <fcebk17@gmail.com>
2021-12-29 19:24:35 +0800

    更改使用者密碼名稱

--- 軟體工程專案/php/user_validation.php
+++ 軟體工程專案/php/user_validation.php
@@ -1,9 +1,9 @@
 <?php
     include "db_conn_software.php";
     $user_account = $_REQUEST["user_account"];
-    $password = $_REQUEST["password"];
+    $user_password = $_REQUEST["user_password"];
 
-    $query = ("SELECT user_account FROM customer WHERE user_account = ? AND password = ?");
+    $query = ("SELECT user_account FROM customer WHERE user_account = ? AND user_password = ?");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(array($user_account,$password)); //執行sql語法
     $result = $stmt->fetchAll();
