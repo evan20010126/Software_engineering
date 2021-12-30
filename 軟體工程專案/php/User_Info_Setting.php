677ae3773193cd2810257c01c1f90051083dddac
YiKaiWu1125 <94746828+YiKaiWu1125@users.noreply.github.com>
2021-12-30 10:49:30 +0800

    更新使用者資訊
    http://localhost/User_Info_Setting.php?user_password=999999999999999&nickname=hihi&email=kkkabcd&phone=12345&birthday=1111&user_account=438

--- 軟體工程專案/php/User_Info_Setting.php
+++ 軟體工程專案/php/User_Info_Setting.php
@@ -7,10 +7,12 @@
     $email = $_REQUEST["email"];
     $phone = $_REQUEST["phone"];
     $birthday = $_REQUEST["birthday"];
+    
     try{
-        $query = ("INSERT INTO customer(user_account, user_password, nickname, email, phone,birthday) VALUES(?,?,?,?,?,?)");
+        //echo($user_account);
+        $query = ("UPDATE customer SET  user_password=?, nickname=?,email=?,phone=? ,birthday=? WHERE user_account =?");
         $stmt = $db->prepare($query);    //db為db_conn.php新建的連線物件 
-        $error = $stmt->execute(array($user_account,$user_password,$nickname,$email,$phone,$birthday));
+        $error = $stmt->execute(array($user_password,$nickname,$email,$phone,$birthday,$user_account));
         $bool = 1;
         echo json_encode($bool);
     }
@@ -20,4 +22,5 @@
     }
             //echo json_encode($result);
     //預設不用回傳值
+    //$result = $stmt->fetchAll();
 ?>
