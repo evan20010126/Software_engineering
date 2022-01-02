a6d7f9f79c442876bb672f42639f48d631f8e74c
YeeOreo <95005853+YeeOreo@users.noreply.github.com>
2022-01-03 02:54:53 +0800

    2022/1/3_push2 登入功能(待續)
    customer_sign_in.html 大幅修改(關於Jquery連結Ajax部分還在研究)
    user_validation.php 感覺沒有想像中只傳布林值這麼簡單，後續會大幅修改來測試功能(session、header跳轉頁面、為了在前端用Ajax，後端需傳回JSON格式)

--- 軟體工程專案/php/user_validation.php
+++ 軟體工程專案/php/user_validation.php
@@ -5,11 +5,17 @@
 
     $query = ("SELECT user_account FROM customer WHERE user_account = ? AND user_password = ?");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(array($user_account,$password)); //執行sql語法
+    $error = $stmt->execute(array($user_account,$user_password)); //執行sql語法
     $result = $stmt->fetchAll();
-    $bool = 0;
+    $bool = "False";
+    // PRINT $user_account;     /*偵錯用：有沒有抓到user_account*/
+    // PRINT $user_password;    /*偵錯用：有沒有抓到user_password*/
+    // PRINT $result;           /*偵錯用：有沒有抓到reslut*/
     if($result!=NULL){
-        $bool = 1;
+        $bool = "True";
     } 
-    echo json_encode($bool);
+    echo '{"success":'.$bool.'}';
+    // echo ($bool); //json_encode
+    header("Location:")
+    // header("Location:javascript://history.go(-1)");     /*讓前端來執行此php後，返回原本之頁面*/
 ?>
