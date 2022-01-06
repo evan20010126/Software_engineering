0e6a8ee547fac018cd4cc872bd81d4fa6a86bb5d
fcebk17 <fcebk17@gmail.com>
2022-01-07 02:05:28 +0800

    使用者查看菜單介面

---
+++ 軟體工程專案/php/user_display_menu.php
@@ -0,0 +1,9 @@
+<?php
+    include "db_conn_software.php";
+    
+    $query = ("SELECT product_id, product_name, product_info, product_pic, product_price FROM product WHERE product_supply = 1");    
+    $stmt = $db->prepare($query);           //db為db_conn.php新建的連線物件 
+    $error = $stmt->execute();
+    $result = $stmt->fetchAll();                //將所有搜尋結果存於result
+    echo json_encode($result);
+?>
