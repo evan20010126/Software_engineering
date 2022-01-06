2971730fbab3434d0f80e1af7699fff68bf05e78
fcebk17 <fcebk17@gmail.com>
2022-01-07 01:43:51 +0800

    更新supply.php

--- 軟體工程專案/php/supply.php
+++ 軟體工程專案/php/supply.php
@@ -1,10 +1,18 @@
 <?php
     include "db_conn_software.php";
-	$product_id = $_POST["product_id"];//Line3,4前端需與後端對照
-	$product_supply = $_POST["product_supply"];
+	$product_name = $_REQUEST['product_name'];//Line3,4前端需與後端對照
+	$product_supply = $_REQUEST["product_supply"];
 
+	/*----- 用product_name搜尋product_id -----*/
+	$query = ("SELECT product_id FROM product WHERE product_name = ?");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($product_name)); //執行sql語法
+    $result = $stmt->fetchAll();
+    $val = $result[0][0];
+	$product_id = $val;
+
+	/*----- 更新產品供需 -----*/
 	$query = ("UPDATE product SET product_supply=? WHERE product_id=?");
-
 	$stmt = $db->prepare($query);
 	$result=$stmt->execute(array($product_supply,$product_id));
 ?>
