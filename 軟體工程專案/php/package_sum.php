d713d6defdf5f6aad36484ae936ea19d7cd664a9
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 07:05:21 +0800

    當賣家改變價格 購物車內的東西也都會自動改變價格了

--- 軟體工程專案/php/package_sum.php
+++ 軟體工程專案/php/package_sum.php
@@ -4,14 +4,26 @@
 
     $user_account = $_REQUEST["q"]; //web passing URL q=
     $coupon_code = $_REQUEST["s"]; //web passing URL s=
-    $query = ("SELECT cost FROM package WHERE user_account =  ? ");
-    $stmt = $db->prepare($query);           //db為db_conn.php新建的連線物件 
-    /*$error = $stmt->execute();*/
-    $error = $stmt->execute(array($user_account)); //用array去傳進?
-    $result = $stmt->fetchAll();                //將所有搜尋結果存於result
+     
+     $query = ("SELECT  product_id01, product_id02, product_id03, product_id04, product_id05, product_id06, product_id07, product_id08, product_id09, product_id10 FROM package WHERE user_account =  ? ");
+     $stmt = $db->prepare($query);           //db為db_conn.php新建的連線物件 
+     /*$error = $stmt->execute();*/
+     $error = $stmt->execute(array($user_account)); //用array去傳進?
+     $result = $stmt->fetchAll();                //將所有搜尋結果存於result
+     /*---------------------connect database & prepare statement----------------------*/
+     $nowtotal = 0;
+     for ($i = 0; $i < 10 && $result[0][$i]!=NULL ; $i++) 
+     {
+        //array_push($product_arr, $result[0][$i]);  
+        $query = ("SELECT product_price FROM product WHERE product_id  =  ? ");
+        $stmt = $db->prepare($query);
+        $error = $stmt->execute(array($result[0][$i])); //product_arr本身就是陣列
+        $newresult = $stmt->fetchAll();
+        $nowtotal += $newresult[0][0];
+     }
+                //將所有搜尋結果存於result
     /*---------------------connect database & prepare statement----------------------*/
-    if($result != NULL){
-        $nowtotal = $result[0][0];
+    if($result[0][0] != NULL){
         if ($coupon_code != NULL){
             $query = ("SELECT  * FROM coupon WHERE coupon_code = ? ");
             $stmt = $db->prepare($query);
