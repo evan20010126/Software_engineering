a338dc567479c1cf6f9c1f2392ac26b1c068178e
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 06:45:12 +0800

    big update
    ouput:
    product_name 商品名稱
    product_price   單一個價格
    product_number 數量

--- 軟體工程專案/php/display_package.php
+++ 軟體工程專案/php/display_package.php
@@ -18,30 +18,27 @@
     //echo json_encode($product_arr);
     //$output_arr = array();
     for($i = 0 ; $i <count($product_arr); $i++){
-        $query = ("SELECT  product_name,product_price FROM product WHERE product_id = ? ");
-        $stmt = $db->prepare($query);
-        $error = $stmt->execute(array($product_arr[$i])); //product_arr本身就是陣列
-        //echo($product_arr[$i])
-        $result = $stmt->fetchAll();
-        $output_arr[$i]["product_name"] = $result[0][0];
-        $output_arr[$i]["product_price"] = $result[0][1];
-        //array_push($output_arr, array($result[0][0],$result[0][1]));
+        if($product_arr[$i]!=-999999){
+            $counter=1;
+            for($j = 0; $j<count($product_arr); $j++){
+                if($product_arr[$i]==$product_arr[$j]&&$i!=$j){
+                    $counter++;
+                    $product_arr[$j]=-999999;
+                }
+            }
+            $query = ("SELECT  product_name,product_price FROM product WHERE product_id = ? ");
+            $stmt = $db->prepare($query);
+            $error = $stmt->execute(array($product_arr[$i])); //product_arr本身就是陣列
+            $result = $stmt->fetchAll();
+            $output_arr[$i]["product_name"] = $result[0][0];
+            $output_arr[$i]["product_price"] = $result[0][1];
+            $output_arr[$i]["product_number"] = $counter; 
+            
+            $product_arr[$i]=-999999;
+        }
     }
     echo json_encode($output_arr);
  
-    /*if(count($product_arr) != 0){ //如果購物車裡面有東西才抓說產品的名稱跟價錢
-        // $product_arr
-        $space = "?"; //因為一定有東西才會跑進來
-        for ($i = 1; $i < count($product_arr); $i++){
-            $space .= " OR product_id = ? ";
-        }
-        $query = ("SELECT  product_name,product_price FROM product WHERE product_id = " . $space);
-        $stmt = $db->prepare($query);
-        $error = $stmt->execute($product_arr); //product_arr本身就是陣列
-        $result = $stmt->fetchAll();
-
-    }
-    echo json_encode($result);*/
     // json_encode
     //[{product_name,product_price} , {...}, {...}]
     //cout << 購物車內的product_
