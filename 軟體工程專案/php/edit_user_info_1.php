d78dfea6eb1f3fd0e092dae21f3c55471014f9c9
YeeOreo <95005853+YeeOreo@users.noreply.github.com>
2022-01-16 08:56:32 +0800

    修改後端edit_user_info_1和2
    兩者PHP皆為Account_Setting.html所用
    edit_user_info_1 SELECT多回傳user_account
    edit_user_info_2只有新增註解說明

--- 軟體工程專案/php/edit_user_info_1.php
+++ 軟體工程專案/php/edit_user_info_1.php
@@ -3,7 +3,7 @@
 
     $user_account = $_REQUEST['user_account']; //前端需同步
 
-    $query = ("SELECT email, nickname, phone, birthday, preference FROM customer WHERE user_account =?");
+    $query = ("SELECT user_account, email, nickname, phone, birthday, preference FROM customer WHERE user_account =?");
     
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(array($user_account)); //執行sql語法
--- 軟體工程專案/php/edit_user_info_2.php
+++ 軟體工程專案/php/edit_user_info_2.php
@@ -1,6 +1,12 @@
 <?php
     include "db_conn_software.php";
-
+    $_POST = json_decode(file_get_contents("php://input"),true);
+    // echo "<pre>";    //想要印出前端傳回的陣列資料可以這樣寫(在前端f12的Network的Response可以看到) by石貫志
+    // print_r($_POST);
+    // print_r($_POST['user_account']);
+    // print_r($_POST['data']['user_account']); //倘若前段是2維陣列這樣用
+    // echo "</pre>";   //想要印出前端傳回的陣列資料可以這樣寫(在前端f12的Network的Response可以看到) by石貫志
+   
     $user_account = $_POST['user_account'];
     $email = $_POST['email'];
     $nickname = $_POST['nickname'];
