72546d9861fe42e504df1acb6d300b70daf8726a
fcebk17 <fcebk17@gmail.com>
2022-01-26 02:07:58 +0800

    黑名單功能

---
+++ 軟體工程專案/php/boss_show_isblank.php
@@ -0,0 +1,30 @@
+<?php
+    include "db_conn_software.php";
+    
+    $user_account = "tzuyu123";//$_REQUEST("user_account");
+
+    $query = ("SELECT is_blank FROM customer WHERE user_account = ?");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($user_account)); //執行sql語法
+    $result = $stmt->fetchAll();
+    $val = $result[0][0];
+
+    if($val == 1){
+        $query = ("UPDATE customer SET is_blank = 0 WHERE user_account = ?");
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($user_account)); //執行sql語法
+    }
+
+    else{
+        $query = ("UPDATE customer SET is_blank = 1 WHERE user_account = ?");
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($user_account)); //執行sql語法
+    }
+
+    $query = ("SELECT is_blank FROM customer WHERE user_account = ?");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($user_account)); //執行sql語法
+    $result = $stmt->fetchAll();
+    echo json_encode($result);
+?>
+
