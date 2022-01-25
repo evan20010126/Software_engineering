d68f4dd76d13b84460ff26623b649cfb16ba1d55
fcebk17 <fcebk17@gmail.com>
2022-01-26 02:13:15 +0800

    黑名單功能

--- 軟體工程專案/php/boss_show_isblank.php
+++ 軟體工程專案/php/boss_show_isblank.php
@@ -1,7 +1,7 @@
 <?php
     include "db_conn_software.php";
     
-    $user_account = "tzuyu123";//$_REQUEST("user_account");
+    $user_account = $_REQUEST("user_account");
 
     $query = ("SELECT is_blank FROM customer WHERE user_account = ?");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
