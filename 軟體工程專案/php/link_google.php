fc90a382a6aab2a2e88650e7ff0127baa048ca87
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-26 02:11:18 +0800

    Update link_google.php

--- 軟體工程專案/php/link_google.php
+++ 軟體工程專案/php/link_google.php
@@ -1,5 +1,8 @@
 <?php
 require_once('vendor/autoload.php');
+header('Access-Control-Allow-Origin:*' );
+header('Access-Control-Allow-Methods:*' );
+
 //header('Content-Type: application/json; charset=UTF-8');
 /*
 //C:\xampp\php\PEAR download ->  composer require google/apiclient # apt install composer
