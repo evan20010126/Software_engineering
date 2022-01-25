addcd57b076c52c3f078c798fef3c9dcc8496cd2
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-26 01:13:31 +0800

    Update customer_manage.php

--- 軟體工程專案/php/customer_manage.php
+++ 軟體工程專案/php/customer_manage.php
@@ -4,7 +4,7 @@
     $phone = $_REQUEST['phone']; //前端需同步
     $birthday  = $_REQUEST['birthday']; //前端需同步
     $string = "";
-    if($nickname!=" "){
+    if(strval($nickname)!=strval(" ")){
         if($string!=""){
             $string=$string." AND ";
         }
@@ -13,34 +13,30 @@
         $string=$string.$nickname;
         $string=$string."'";
     }
-    if($phone!=" "){
+    if(strval($phone)!=strval(" ")){
         if($string!=""){
             $string=$string." AND ";
         }
         $string=$string." phone = ".$phone;
     }
-    if($birthday!=" "){
+    if(strval($birthday)!=strval(" ")){
         if($string!=""){
             $string=$string." AND ";
         }
         $string=$string." birthday = ".$birthday;
     }
-    if($nickname==" "){
-        if($nickname==" "){
-            if($birthday==" "){
-                echo json_encode(NULL);
-                exit;
-            }
-        }
+    if(strval($string)==strval("")){
+        echo json_encode(NULL);
+            exit;
     }
     
-    $query = ("SELECT user_account, email, nickname, phone, birthday, preference FROM customer WHERE ".$string );
-    //$query = ("SELECT user_account, email, nickname, phone, birthday, preference FROM customer WHERE nickname = 'hihi'");
+    //$query = ("SELECT user_account, email, nickname, phone, birthday, preference FROM customer WHERE ".$string );
+    $query = ("SELECT user_account, email, nickname, phone, birthday, preference FROM customer WHERE nickname = 22222");
     //echo($query);
 
     $stmt = $db->prepare($query);        //db為db_conn.php新建的連線物件 
     $error = $stmt->execute();
     $result = $stmt->fetchAll();        //將所有搜尋結果存於result
-
-    echo json_encode($result);
+    if($result==NULL) echo json_encode(NULL);
+    else echo json_encode($result);
 ?>
